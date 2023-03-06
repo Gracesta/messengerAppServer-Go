a32ad30fc6ca41acd1924cbff1ab2f1cc00b0905
Gracesta <jayblizrage@gmail.com>
2023-03-06 23:25:33 +0900

    readme

--- client.go
+++ client.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -42,19 +45,132 @@ func (client *Client) Run() {
 
 		switch client.flag {
 		case 1:
-			fmt.Println("1. Public Chat")
+			client.PublicChat()
 			break
 		case 2:
-			fmt.Println("2. Private Chat")
+			client.PrivateChat()
 			break
 		case 3:
-			fmt.Println("3. Change Username")
-			break
-		case 0:
-			fmt.Println("0. Exit")
+			client.UpdateName()
 			break
 		}
 	}
+}
+
+func (client *Client) SelectUser() {
+
+	// send who to know the user online
+	_, err := client.conn.Write([]byte("who\n"))
+	if err != nil {
+		fmt.Println("client conn.write error: ", err)
+		return
+	}
+}
+
+func (client *Client) PrivateChat() {
+
+	// show online user first
+	client.SelectUser()
+
+	var targetUserName string
+	var chatMsg string
+
+	fmt.Println(">>>>> Input the name of user who you wanna chat with")
+	fmt.Scanln(&targetUserName)
+
+	for targetUserName != "exit" {
+		var errBufio error
+
+		fmt.Println(">>>>> Input the message you wanna send (input \"exit\" to exit)")
+		in := bufio.NewReader(os.Stdin)
+		chatMsg, errBufio = in.ReadString('\n')
+		if errBufio != nil {
+			fmt.Println("reading string error", errBufio)
+		}
+		// fmt.Scanln(&chatMsg)
+
+		// send message to usr conn to broadcast
+		for chatMsg != "exit\r\n" {
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + targetUserName + "|" + chatMsg + "\n"
+				_, err := client.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("client conn.write error: ", err)
+					break
+				}
+
+				chatMsg = ""
+				fmt.Println(">>>>> Input the message you wanna send (input \"exit\" to exit)")
+				chatMsg, errBufio = in.ReadString('\n')
+				if errBufio != nil {
+					fmt.Println("reading string error", errBufio)
+				}
+				// fmt.Scanln(&chatMsg)
+			}
+		}
+
+		client.SelectUser()
+		fmt.Println(">>>>> Input the name of user who you want chat with (input \"exit\" to exit)")
+		fmt.Scanln(&targetUserName)
+	}
+}
+
+func (client *Client) PublicChat() {
+	var chatMsg string
+	var errBufio error
+	in := bufio.NewReader(os.Stdin)
+
+	fmt.Println(">>>>> Input the message you want to send (input \"exit\" to exit)")
+	chatMsg, errBufio = in.ReadString('\n')
+	if errBufio != nil {
+		fmt.Println("reading string error", errBufio)
+	}
+	// fmt.Scanln(&chatMsg)
+
+	for chatMsg != "exit\r\n" {
+
+		// send message to usre conn to broadcast
+		if len(chatMsg) != 0 {
+			sendMsg := chatMsg
+			_, err := client.conn.Write([]byte(sendMsg))
+			if err != nil {
+				fmt.Println("client conn.write error: ", err)
+				break
+			}
+
+		}
+
+		chatMsg = ""
+		fmt.Println(">>>>> Input the message you want to send (input \"exit\" to exit)")
+		chatMsg, errBufio = in.ReadString('\n')
+		if errBufio != nil {
+			fmt.Println("reading string error", errBufio)
+		}
+		// fmt.Scanln(&chatMsg)
+	}
+}
+
+func (client *Client) UpdateName() bool {
+	fmt.Println("Input the new username you want")
+	fmt.Scanln(&client.Name)
+
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("client conn.write error: ", err)
+		return false
+	}
+
+	return true
+
+}
+
+func (client *Client) HandleResponse() {
+	// handle the message from server
+
+	// Once read message from conn, display it
+	io.Copy(os.Stdout, client.conn)
+
 }
 
 func (client *Client) menu() bool {
@@ -62,7 +178,7 @@ func (client *Client) menu() bool {
 
 	fmt.Println("1. Public Chat")
 	fmt.Println("2. Private Chat")
-	fmt.Println("3. Change Username")
+	fmt.Println("3. Update Username")
 	fmt.Println("0. Exit")
 
 	fmt.Scanln(&flag)
@@ -102,6 +218,9 @@ func main() {
 	} else {
 		fmt.Println("Input number within range")
 	}
+
+	// go routin for handling message from server
+	go client.HandleResponse()
 
 	client.Run()
 }
